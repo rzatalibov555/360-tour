acfd2a0c7536902bec52edea3cef696732d408c1
rzatalibov555 <sethubpro@gmail.com>
2023-04-13 17:06:27 +0400

    info

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,12 +8,31 @@
     <title>Document</title>
     <script src="https://pchen66.github.io/js/three/three.min.js"></script>
     <script src="https://rawgit.com/pchen66/panolens.js/dev/build/panolens.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/index.css">
 </head>
+
 <body>
 
-    <div id="container"></div>
-    
+    <div id="containers">
+        <div class="tt">
+            <img data-bs-toggle="tooltip" title="Information!"
+                src="https://static.vecteezy.com/system/resources/thumbnails/005/747/906/small/info-icon-template-information-icon-colorful-free-vector.jpg"
+                alt="">
+        </div>
+    </div>
+
 </body>
+
 </html>
 <script src="js/main.js"></script>
+<script>
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+</script>
--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var panorama, viewer, container, infospot;
 
-container = document.querySelector( '#container' );
+container = document.querySelector( '#containers' );
+
+
+
 
 // panorama = new PANOLENS.ImagePanorama( 'img/abandoned_tiled_room_4k.jpg' );
 // panorama = new PANOLENS.ImagePanorama( 'https://museumsformoove.s3.amazonaws.com/Miniature+Book+Museum/1.jpg' );
@@ -8,7 +11,7 @@ panorama = new PANOLENS.ImagePanorama( 'img/1.jpg' );
 
 infospot = new PANOLENS.Infospot( 350, PANOLENS.DataImage.Info );
 infospot.position.set( 0, 0, -5000 );
-infospot.addHoverText( 'Hello Panolens', 30 );
+// infospot.addHoverText( 'Hello Panolens', 50 );
 panorama.add( infospot );
 
 viewer = new PANOLENS.Viewer( { container: container } );
